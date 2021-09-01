7f9e2fe26dfde0bdb6ff4ed211c3d3f01aecd8df
Tshepo Tsilo <tshepo9099@gmail.com>
2021-09-01 14:38:56 +0200

    added grapejs-react library

--- site-builder/src/components/Builder/Builder.js
+++ site-builder/src/components/Builder/Builder.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import 'grapesjs/dist/css/grapes.min.css';
-import grapesjs from 'grapesjs';
+import React from "react";
+import "grapesjs/dist/css/grapes.min.css";
+import grapesjs from "grapesjs";
 // If you need plugins, put them below the main grapesjs script
 // import 'grapesjs-some-plugin';
-import './Builder.css';
+import "./Builder.css";
 
 function Builder() {
   // Initiate the GrapeJS editor
   const editor = grapesjs.init({
     // Indicate where to init the editor. You can also pass an HTMLElement
-    container: '#gjs',
+    container: "#gjs",
     // Get the content for the canvas directly from the element
     // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
     fromElement: true,
     // Size of the editor
-    height: '300px',
-    width: 'auto',
+    height: "300px",
+    width: "auto",
     // Disable the storage manager for the moment
     storageManager: false,
     // Avoid any default panel
     panels: { defaults: [] },
     blockManager: {
-      appendTo: '#blocks',
+      appendTo: "#blocks",
       blocks: [
         {
-          id: 'section', // id is mandatory
-          label: '<b>Section</b>', // You can use HTML/SVG inside labels
-          attributes: { class: 'gjs-block-section' },
+          id: "section", // id is mandatory
+          label: "<b>Section</b>", // You can use HTML/SVG inside labels
+          attributes: { class: "gjs-block-section" },
           content: `<section>
           <h1>This is a simple title</h1>
           <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
         </section>`,
         },
         {
-          id: 'text',
-          label: 'Text',
+          id: "text",
+          label: "Text",
           content: '<div data-gjs-type="text">Insert your text here</div>',
         },
         {
-          id: 'image',
-          label: 'Image',
+          id: "image",
+          label: "Image",
           // Select the component once it's dropped
           select: true,
           // You can pass components as a JSON instead of a simple HTML string,
           // in this case we also use a defined component type `image`
-          content: { type: 'image' },
+          content: { type: "image" },
           // This triggers `active` event on dropped components and the `image`
           // reacts by opening the AssetManager
           activate: true,
@@ -52,6 +52,7 @@ function Builder() {
       ],
     },
   });
+
   return (
     <div>
       <div id='gjs'>
