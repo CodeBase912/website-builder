5c0c99c5c41ed669ede1ebb12c00792421b420c6
Tshepo Tsilo <tshepo9099@gmail.com>
2022-04-28 14:22:56 +0200

    creates Builder page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // Import Custom React Components
 import HomePage from "./pages/home/HomePage";
 import Login from "./pages/login/Login";
+import Builder from "./pages/builder/Builder";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/builder" element={<Builder />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/builder/Builder.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "grapesjs/dist/css/grapes.min.css";
+import grapesjs from "grapesjs";
+import Header from "../../components/site/Header";
+// Import Custom React Components
+
+const Builder = () => {
+  return (
+    <div>
+      <Header />
+      <main></main>
+    </div>
+  );
+};
+
+export default Builder;
