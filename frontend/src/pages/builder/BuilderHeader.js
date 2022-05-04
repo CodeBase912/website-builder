8f1968b33c7f9767067d7dbe46e44f95026725f5
Tshepo Tsilo <tshepo9099@gmail.com>
2022-05-05 01:42:31 +0200

    add: CanvasWidthInput component that contains all the functionality of the #canvas-width-input element

--- frontend/src/pages/builder/BuilderHeader.js
+++ frontend/src/pages/builder/BuilderHeader.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
 import classNames from "classnames";
 // Import Custom React Components
-import Button from "../../components/common/Button";
+import CanvasWidthInput from "./CanvasWidthInput";
 // Import Icons
-import NavBar from "../../components/site/NavBar";
 import Logo from "../../components/site/Logo";
 import { Icons } from "../../components/common/icons/icons";
-import Input from "../../components/common/forms/Input";
-import { findActiveDevice } from "../../util/canvas-util";
 
 const BuilderHeader = ({
   fixed = true,
@@ -15,86 +12,6 @@ const BuilderHeader = ({
   dropDown = false,
   editor,
 }) => {
-  const [canvasWidth, setCanvasWidth] = useState(null);
-  const [activeDevice, setActiveDevice] = useState("Desktop");
-  useEffect(() => {
-    if (editor) {
-      //  Get canvas wrapper to which we will obverse
-      const canvasWrapper = editor.Canvas.getFrame().view.wrapper.$el[0];
-      //  Get canvas width input element
-      const canvasWidthInput = document.querySelector("#canvas-width-input");
-
-      //  Get available device from the editor instance
-      const availableDevices = editor.Devices.getAll().models.map(
-        (device, deviceIndex) => {
-          return [device.attributes.priority, device.id];
-        }
-      );
-
-      // Define the variable that will hold numeric ID that will bw
-      // used by the clearTimeOut function to reset the setTimeOut timer
-      let timeOutFunctionId;
-      //  Use the resize observer to handle canvas width change
-      const resize_ob = new ResizeObserver(function (entries) {
-        // since we are observing only a single element, so we access the first element in entries array
-        let rect = entries[0].contentRect;
-
-        // current width & height
-        let width = rect.width;
-        let height = rect.height;
-
-        // clearTimeOut() resets the setTimeOut() timer
-        // due to this the function in setTimeout() is
-        // fired after we are done resizing the canvas
-        clearTimeout(timeOutFunctionId);
-        // Variable that will hold the active device after canvas resize
-        let device;
-
-        // setTimeout returns the numeric ID which is used by
-        // clearTimeOut to reset the timer
-        timeOutFunctionId = setTimeout((testVar) => {
-          // Set the active device after canvas resize
-          device = findActiveDevice(width, availableDevices);
-
-          // Check if the active devie has change
-          if (activeDevice != device) {
-            // Active device changed, update styles
-            const previousDeviceBtn = document.querySelector(
-              `#device-${activeDevice.toLowerCase()}-container`
-            );
-            previousDeviceBtn.style = "";
-
-            const selectedDeviceBtn = document.querySelector(
-              `#device-${device.toLowerCase()}-container`
-            );
-            selectedDeviceBtn.style.backgroundColor = "rgba(15, 15, 15, 0.7)";
-            selectedDeviceBtn.style.color = "#00edff";
-            selectedDeviceBtn.style.fill = "#00edff";
-            selectedDeviceBtn.style.stroke = "#00edff";
-
-            // Update active device state variable
-            setActiveDevice(device);
-          }
-        }, 100);
-
-        // Update #canvas-width-input value
-        canvasWidthInput.value = Math.floor(width);
-      });
-
-      // start observing for resize
-      resize_ob.observe(canvasWrapper);
-
-      return () => {
-        // stop observing for resize
-        resize_ob.unobserve(canvasWrapper);
-      };
-    }
-
-    return () => {
-      //
-    };
-  }, [editor, canvasWidth, activeDevice]);
-
   return (
     <header
       className={classNames(
@@ -114,8 +31,7 @@ const BuilderHeader = ({
         </div>
       </div>
 
-      {/* Nav Bar Container */}
-      {/* <NavBar absolute={fixed} /> */}
+      {/* Builder Header Buttons Container */}
       <div className="flex w-full">
         {/* Toggle Borders Button Container */}
         <div className="relative flex">
@@ -130,7 +46,7 @@ const BuilderHeader = ({
             </div>
           </div>
 
-          {/* Canvas Width Adjust Toggler */}
+          {/* Toggle Canvas Width Handles Container  */}
           <div id="canvas-width-adjust-container">
             <div
               id="canvas-width-adjuster-toggler"
@@ -142,6 +58,7 @@ const BuilderHeader = ({
             </div>
           </div>
 
+          {/* Devices Panel Container */}
           <div className="flex border-x border-grey">
             {/* Select Device Buttons Container */}
             <div id="devices-panel-container" className="flex">
@@ -176,87 +93,9 @@ const BuilderHeader = ({
 
             {/* Canvas Width Display/Input & Options Container  */}
             <div className="flex">
-              {/* Canvas Width Display/Input Container */}
-              <div className="flex items-center px-5 group hover:bg-dark">
-                <div className="flex justify-between min-w-[97px] w-[97px] px-3 border border-grey py-1 h-fit bg-transparent my-auto">
-                  <input
-                    id="canvas-width-input"
-                    onSubmit={(e) => {
-                      console.log("Width: ", e.target.value);
-                    }}
-                    onKeyDown={(e) => {
-                      //   e.preventDefault();
-                      console.log("KeyDown event: ", e);
-                      console.log("Value: ", e.target.value);
-                      console.log("Key: ", e.key);
-                      console.log("State: ", e.getModifierState());
-                      console.log("Parse key: ", parseInt(e.key));
-                      console.log(
-                        "Parse key condition: ",
-                        parseInt(e.key) / 1 === parseInt(e.key)
-                      );
-                      if (parseInt(e.key) / 1 !== parseInt(e.key)) {
-                        // check if key is Enter or backspace
-                        if (
-                          e.key === "Enter" ||
-                          e.key === "Backspace" ||
-                          e.key.includes("Arrow")
-                        ) {
-                        } else {
-                          // Key is not ENTER or BACKSPACE, prevent default
-                          e.preventDefault();
-                        }
-                      }
-                    }}
-                    onKeyUp={(e) => {
-                      console.log("onKeyUp event: ", e);
+              {/* Canvas Input */}
+              <CanvasWidthInput editor={editor} />
 
-                      if (e.key === "Enter") {
-                        const canvasWrapper =
-                          document.querySelector(".gjs-frame-wrapper");
-                        // Set the width of the canvas to the current value
-                        const frame_wrapper =
-                          document.querySelector(".gjs-frame-wrapper");
-                        frame_wrapper.style.transition = "0.5 ease-in-out";
-                        const canvasContainer = document.querySelector(
-                          ".gjs-cv-canvas__frames"
-                        );
-                        let newCanvasWidth = e.target.value;
-                        if (newCanvasWidth < 100) {
-                          newCanvasWidth = 100;
-                          e.target.value = Math.floor(
-                            canvasWrapper.getBoundingClientRect().width
-                          );
-                          return;
-                        } else if (
-                          newCanvasWidth >
-                          canvasContainer.getBoundingClientRect().width - 100
-                        ) {
-                          newCanvasWidth = Math.floor(
-                            canvasContainer.getBoundingClientRect().width
-                          );
-                          e.target.value = newCanvasWidth;
-                          canvasWrapper.style.width = `${newCanvasWidth}px`;
-                          return;
-                        }
-
-                        e.target.value = newCanvasWidth;
-                        canvasWrapper.style.width = `${newCanvasWidth}px`;
-                        // setCanvasWidth(newCanvasWidth);
-                      }
-                    }}
-                    name="canvas-width"
-                    type={"text"}
-                    placeholder={"300"}
-                    defaultValue={canvasWidth}
-                    // value={canvasWidth}
-                    className="w-full text-right pr-3 bg-transparent outline-none text-white"
-                  />
-                  <p className="text-grey-light bg-tranparent cursor-pointer">
-                    {"px"}
-                  </p>
-                </div>
-              </div>
               {/* Options Container */}
               <div className="">
                 <div className="flex items-center justify-center w-16 hover:bg-dark h-full text-2xl text-grey-lighter cursor-pointer">
@@ -267,7 +106,9 @@ const BuilderHeader = ({
           </div>
         </div>
       </div>
-      <div className="relative">Right panel</div>
+
+      {/* Right panel */}
+      <div className="relative"></div>
     </header>
   );
 };
---
+++ frontend/src/pages/builder/CanvasWidthInput.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import { findActiveDevice } from "../../util/canvas-util";
+
+const CanvasInput = ({ editor }) => {
+  const [canvasWidth, setCanvasWidth] = useState(null);
+  const [activeDevice, setActiveDevice] = useState("Desktop");
+  useEffect(() => {
+    if (editor) {
+      //  Get canvas wrapper to which we will obverse
+      const canvasWrapper = editor.Canvas.getFrame().view.wrapper.$el[0];
+      //  Get canvas width input element
+      const canvasWidthInput = document.querySelector("#canvas-width-input");
+
+      //  Get available device from the editor instance
+      const availableDevices = editor.Devices.getAll().models.map(
+        (device, deviceIndex) => {
+          return [device.attributes.priority, device.id];
+        }
+      );
+
+      // Define the variable that will hold numeric ID that will bw
+      // used by the clearTimeOut function to reset the setTimeOut timer
+      let timeOutFunctionId;
+      //  Use the resize observer to handle canvas width change
+      const resize_ob = new ResizeObserver(function (entries) {
+        // since we are observing only a single element, so we access the first element in entries array
+        let rect = entries[0].contentRect;
+
+        // current width & height
+        let width = rect.width;
+        let height = rect.height;
+
+        // clearTimeOut() resets the setTimeOut() timer
+        // due to this the function in setTimeout() is
+        // fired after we are done resizing the canvas
+        clearTimeout(timeOutFunctionId);
+        // Variable that will hold the active device after canvas resize
+        let device;
+
+        // setTimeout returns the numeric ID which is used by
+        // clearTimeOut to reset the timer
+        timeOutFunctionId = setTimeout((testVar) => {
+          // Set the active device after canvas resize
+          device = findActiveDevice(width, availableDevices);
+
+          // Check if the active devie has change
+          if (activeDevice != device) {
+            // Active device changed, update styles
+            const previousDeviceBtn = document.querySelector(
+              `#device-${activeDevice.toLowerCase()}-container`
+            );
+            previousDeviceBtn.style = "";
+
+            const selectedDeviceBtn = document.querySelector(
+              `#device-${device.toLowerCase()}-container`
+            );
+            selectedDeviceBtn.style.backgroundColor = "rgba(15, 15, 15, 0.7)";
+            selectedDeviceBtn.style.color = "#00edff";
+            selectedDeviceBtn.style.fill = "#00edff";
+            selectedDeviceBtn.style.stroke = "#00edff";
+
+            // Update active device state variable
+            setActiveDevice(device);
+          }
+        }, 100);
+
+        // Update #canvas-width-input value
+        canvasWidthInput.value = Math.floor(width);
+      });
+
+      // start observing for resize
+      resize_ob.observe(canvasWrapper);
+
+      return () => {
+        // stop observing for resize
+        resize_ob.unobserve(canvasWrapper);
+      };
+    }
+  }, [editor, canvasWidth, activeDevice]);
+
+  /**
+   * allows only numbers to be entered into the input field
+   * @param {} e
+   */
+  const onKeyDown = (e) => {
+    //   e.preventDefault();
+    // console.log("KeyDown event: ", e);
+    // console.log("Value: ", e.target.value);
+    // console.log("Key: ", e.key);
+    // console.log("State: ", e.getModifierState());
+    // console.log("Parse key: ", parseInt(e.key));
+    // console.log(
+    //   "Parse key condition: ",
+    //   parseInt(e.key) / 1 === parseInt(e.key)
+    // );
+    if (parseInt(e.key) / 1 !== parseInt(e.key)) {
+      // check if key is Enter or backspace
+      if (
+        e.key === "Enter" ||
+        e.key === "Backspace" ||
+        e.key.includes("Arrow")
+      ) {
+      } else {
+        // Key is not ENTER or BACKSPACE, prevent default
+        e.preventDefault();
+      }
+    }
+  };
+
+  /**
+   * sets the canvas width to the input field value when Enter key is pressed
+   * @param {*} e
+   * @returns
+   */
+  const onKeyUp = (e) => {
+    console.log("onKeyUp event: ", e);
+
+    if (e.key === "Enter") {
+      const canvasWrapper = document.querySelector(".gjs-frame-wrapper");
+      // Set the width of the canvas to the current value
+      const frame_wrapper = document.querySelector(".gjs-frame-wrapper");
+      frame_wrapper.style.transition = "0.5 ease-in-out";
+      const canvasContainer = document.querySelector(".gjs-cv-canvas__frames");
+      let newCanvasWidth = e.target.value;
+      if (newCanvasWidth < 100) {
+        newCanvasWidth = 100;
+        e.target.value = Math.floor(
+          canvasWrapper.getBoundingClientRect().width
+        );
+        return;
+      } else if (
+        newCanvasWidth >
+        canvasContainer.getBoundingClientRect().width - 100
+      ) {
+        newCanvasWidth = Math.floor(
+          canvasContainer.getBoundingClientRect().width
+        );
+        e.target.value = newCanvasWidth;
+        canvasWrapper.style.width = `${newCanvasWidth}px`;
+        return;
+      }
+
+      e.target.value = newCanvasWidth;
+      canvasWrapper.style.width = `${newCanvasWidth}px`;
+      // setCanvasWidth(newCanvasWidth);
+    }
+  };
+
+  return (
+    <div className="flex items-center px-5 group hover:bg-dark">
+      <div className="flex justify-between min-w-[97px] w-[97px] px-3 border border-grey py-1 h-fit bg-transparent my-auto">
+        <input
+          id="canvas-width-input"
+          onSubmit={(e) => {
+            console.log("Width: ", e.target.value);
+          }}
+          onKeyDown={onKeyDown}
+          onKeyUp={onKeyUp}
+          name="canvas-width"
+          type={"text"}
+          placeholder={"300"}
+          defaultValue={canvasWidth}
+          // value={canvasWidth}
+          className="w-full text-right pr-3 bg-transparent outline-none text-white"
+        />
+        <p className="text-grey-light bg-tranparent cursor-pointer">{"px"}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CanvasInput;
