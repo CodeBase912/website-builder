13f61ebe262c4d19fc9b079b8cd5a3bc5ca2538d
Tshepo Tsilo <tshepo9099@gmail.com>
2023-07-05 20:36:38 +0200

    feat: close modal on click of modal overlay background

--- frontend/src/pages/builder/SideBarContent.js
+++ frontend/src/pages/builder/SideBarContent.js
@@ -18,72 +18,91 @@ const SideBarContent = ({ sideBarExpanded, setSideBarExpanded }) => {
   // const { state, updateSideBarExpanded } = useContext(WebXBuilderContext);
   // console.log(`\n ${state}  \n `);
 
-  const handleClose = () => {
+  const handleClose = (e) => {
     console.log("Handle close worked");
+    console.log("click event: ", e);
     setSideBarExpanded(false);
   };
   return (
     /* Side Bar Content Container */
     <div
+      id="side-bar-content__overlay"
       className={classNames(
-        { hidden: sideBarExpanded === false },
-        "flex flex-col shadow-2xl shadow-grey rounded-sm bg-white absolute left-20 top-[50%] translate-y-[-50%] z-10 h-[95%] max-h-[95%]"
+        { "bg-opacity-10": sideBarExpanded === true },
+        { "bg-opacity-0": sideBarExpanded === false },
+        "transition-all duration-300",
+        "bg-gray-900 absolute top-[50%] translate-y-[-50%] z-10 w-full h-full"
       )}
+      onClick={(e) => {
+        // TODO: This is a quick fix. This is not scalable. Please fix!!!
+        if (e.target.closest("#side-bar-content__content-wrapper")) {
+        } else if (e.target.closest("#side-bar-content__overlay")) {
+          setSideBarExpanded(false);
+        }
+      }}
     >
-      {/* Panel Heading Container */}
-      <div className="flex justify-between border-b border-gray-400">
-        {/* Panel Heading */}
-        <h3 className="p-4 text-2xl text-grey no-text-selection font-extrabold pointer-events-none">
-          {"Add Elements"}
-        </h3>
-        {/* Panel Actions Container */}
-        <div className="flex items-center">
-          <div
-            title="Help Center > Add Elements"
-            className="flex justify-center items-center text-lg w-16 h-full hover:bg-gray-200 cursor-pointer"
-          >
-            {Icons.question}
-          </div>
-          <div
-            title="Close"
-            className="flex justify-center items-center text-lg w-16 h-full hover:bg-gray-200 cursor-pointer"
-            onClick={handleClose}
-          >
-            {Icons.closeIcon}
+      <div
+        id="side-bar-content__content-wrapper"
+        className={classNames(
+          { hidden: sideBarExpanded === false },
+          "flex flex-col shadow-2xl shadow-grey rounded-sm bg-white absolute left-20 top-[50%] translate-y-[-50%] z-10 h-[95%] max-h-[95%]"
+        )}
+      >
+        {/* Panel Heading Container */}
+        <div className="flex justify-between border-b border-gray-400">
+          {/* Panel Heading */}
+          <h3 className="p-4 text-2xl text-grey no-text-selection font-extrabold pointer-events-none">
+            {"Add Elements"}
+          </h3>
+          {/* Panel Actions Container */}
+          <div className="flex items-center">
+            <div
+              title="Help Center > Add Elements"
+              className="flex justify-center items-center text-lg w-16 h-full hover:bg-gray-200 cursor-pointer"
+            >
+              {Icons.question}
+            </div>
+            <div
+              title="Close"
+              className="flex justify-center items-center text-lg w-16 h-full hover:bg-gray-200 cursor-pointer"
+              onClick={handleClose}
+            >
+              {Icons.closeIcon}
+            </div>
           </div>
         </div>
-      </div>
-      {/* Panel Content */}
-      <div className="flex w-full flex-auto h-0">
-        {/* Blocks Categories Container */}
-        <div
-          id="blocks-categories-panel"
-          className="flex flex-col min-w-fit h-full border-r border-gray-400"
-        >
-          {/* Category */}
-          {/* <div className="py-3 px-4 font-semibold no-text-selection">
+        {/* Panel Content */}
+        <div className="flex w-full flex-auto h-0">
+          {/* Blocks Categories Container */}
+          <div
+            id="blocks-categories-panel"
+            className="flex flex-col min-w-fit h-full border-r border-gray-400"
+          >
+            {/* Category */}
+            {/* <div className="py-3 px-4 font-semibold no-text-selection">
                 {"QUICK ADD"}
               </div> */}
-          {/* Category */}
-          {/* <div className="py-3 px-4 font-semibold bg-blue-light text-blue-darker no-text-selection">
+            {/* Category */}
+            {/* <div className="py-3 px-4 font-semibold bg-blue-light text-blue-darker no-text-selection">
                 {"TESTIMONIALS"}
               </div> */}
-        </div>
-        {/* Block Elements Here */}
-        <div className="h-full flex-auto overflow-y-scroll custom-scrollbar scrollbar-rounded w-[300px]">
-          {/* Blocks Elements Header */}
-          <h2
-            id="blocks-category-panel-title"
-            className="px-3 py-3 font-bold text-xl no-text-selection"
-          >
-            {/* Panel categories here */}
-          </h2>
-          {/* Block Elements Container */}
-          <div
-            id="blocks-category-panel"
-            className="px-3 pb-3 no-text-selection"
-          >
-            {/* {"Blocks Elements Here"} */}
+          </div>
+          {/* Block Elements Here */}
+          <div className="h-full flex-auto overflow-y-scroll custom-scrollbar scrollbar-rounded w-[300px]">
+            {/* Blocks Elements Header */}
+            <h2
+              id="blocks-category-panel-title"
+              className="px-3 py-3 font-bold text-xl no-text-selection"
+            >
+              {/* Panel categories here */}
+            </h2>
+            {/* Block Elements Container */}
+            <div
+              id="blocks-category-panel"
+              className="px-3 pb-3 no-text-selection"
+            >
+              {/* {"Blocks Elements Here"} */}
+            </div>
           </div>
         </div>
       </div>
